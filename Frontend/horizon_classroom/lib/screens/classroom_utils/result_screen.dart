9c9af6fb06d5e6ba1df8f469a27cb432e0e15664
Kishor404 <kishor404@outlook.in>
2025-07-18 12:33:01 +0530

    Result UI Compeleted

--- Frontend/horizon_classroom/lib/screens/classroom_utils/result_screen.dart
+++ Frontend/horizon_classroom/lib/screens/classroom_utils/result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 class ResultScreen extends StatefulWidget {
@@ -9,6 +10,18 @@ class ResultScreen extends StatefulWidget {
 }
 
 class _ResultScreenState extends State<ResultScreen> {
+
+  String className = "Data Structures and Algorithms";
+
+  int score = 0;
+  int totalScore = 2;
+
+  Map<String, String> results =
+    {
+      "position1":"John Doe",
+      "position2":"Jane Smith",
+      "position3":"Alice Johnson",
+    };
 
   // ================= CHECK LOGIN STATUS =================
 
@@ -34,17 +47,159 @@ class _ResultScreenState extends State<ResultScreen> {
 
   @override
   Widget build(BuildContext context) {
+
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Result Screen")),
+      appBar: AppBar(),
       body: Center(
         child: Column(
           children:[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/classroom');
-              },
-              child: const Text("Go to Classroom"),
+            // ================ CLASS INFO ================
+
+            SizedBox(
+              height: height * 0.1,
+              width: width * 0.8,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Flexible(
+                      child:Text(className,
+                      style: GoogleFonts.amaranth(fontSize: width * 0.053, color: const Color(0xFF08AD8C)),
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                    ),
+                  ),
+                ],
+              ),
             ),
+
+            // =============== AD SPACE =================
+
+            SizedBox(
+              width: width * 0.8,
+              child: Padding( 
+                padding: EdgeInsets.symmetric(vertical: height * 0.04),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+
+                  children: [
+                    SizedBox(
+                      width: width * 0.8,
+                      height: (width * 0.8)*0.5625, // 16:9 aspect ratio
+                      child: ColoredBox(color: Colors.cyan)
+                    )
+                  ],
+                )
+              ),
+            ),
+
+            // ================ YOUR SCORE ================
+
+            SizedBox(
+              width: width * 0.8,
+              height: height * 0.1,
+              child: Text("$score/$totalScore",
+                style: GoogleFonts.amaranth(fontSize: width * 0.1, color: const Color(0xFF08AD8C)),
+                textAlign: TextAlign.center,
+              ),
+            ),
+
+            // ================ RESULTS ================
+
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: height * 0.02),
+              child: SizedBox(
+                width: width * 0.85,
+                height: height * 0.35,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "🏆 Top Performers",
+                      style: GoogleFonts.amaranth(
+                        fontSize: width * 0.05,
+                        fontWeight: FontWeight.bold,
+                        color: const Color(0xFF08AD8C),
+                      ),
+                    ),
+                    SizedBox(height: height * 0.015),
+                    ...results.entries.map((entry) {
+                      int pos = int.parse(entry.key.replaceAll("position", ""));
+                      Color iconColor;
+                      switch (pos) {
+                        case 1:
+                          iconColor = Colors.amber;
+                          break;
+                        case 2:
+                          iconColor = const Color.fromARGB(255, 212, 212, 212);
+                          break;
+                        case 3:
+                          iconColor = const Color.fromARGB(255, 165, 80, 0);
+                          break;
+                        default:
+                          iconColor = Colors.blueGrey;
+                      }
+
+                      return Card(
+                        elevation: 4,
+                        color: const Color(0xFF08AD8C),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        margin: EdgeInsets.symmetric(vertical: height * 0.01),
+                        child: Padding(
+                          padding:  EdgeInsets.symmetric(vertical: height * 0.01 , horizontal: width * 0.03),
+                          child: Row(
+                            children: [
+                              Card(
+                                color: iconColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(360),
+                                ),
+                                child: SizedBox(
+                                  width: width * 0.09,
+                                  height: width * 0.09,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [Text(pos.toString(),
+                                    style: GoogleFonts.amaranth(
+                                      fontSize: width * 0.045,
+                                      color: Colors.black87,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                    )
+                                  ]
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: width * 0.04),
+                              Expanded(
+                                child: Text(
+                                  entry.value,
+                                  style: GoogleFonts.amaranth(
+                                    fontSize: width * 0.045,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ],
+                ),
+              ),
+            ),
+
+
+            
           ],
         )
       ),
